bdb03e379feebf734401ed5a5f5f7b67cd444428
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-09-29 23:28:18 +0800

    3rd commit with angular app

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,40 @@
+// import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// boostrap imports
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+//angular material imports
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    NgbModule,
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+})
+export class AppModule { }
