847bc3a4a31989421ddbf8263b29388f3cee4d19
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-11-04 00:21:49 +0800

    make it betterzzz

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use('/users', users);
 
 app.get('*', (req, res) => {
     //  res.send('<h1>Hello from the Server Side</h1>')
-    res.sendFile(path.join(__dirname + '/app/dist/index.html'),)
+    res.sendFile(path.join(__dirname + '/dist/index.html'),)
 });
 
 
