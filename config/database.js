ceecb34e2d0541e182814714d0a91fe78fbd9472
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-11-04 00:18:21 +0800

    make it bettersxxx

--- config/database.js
+++ config/database.js
@@ -3,7 +3,28 @@ require('dotenv').config()
 const crypto = require('crypto');
 
 
-const hash = crypto.createHmac(process.env.ENCRYPTION_ALGO, process.env.ENCRYPTION_KEY).update('Akeem').digest(process.env.ENCRYPTION_ENCODING);
+// Node.js program to demonstrate the    
+// crypto.createHmac() method
+ 
+// Includes crypto module
+ 
+// Defining key
+const secret = 'meguizo';
+ 
+// Calling createHmac method
+const hash = crypto.createHmac('sha256', secret)
+                    
+                   // updating data
+                   .update('akeem')
+ 
+                   // Encoding to be used
+                   .digest('hex');
+ 
+
+
+
+
+// const hash = crypto.createHmac(process.env.ENCRYPTION_ALGO, process.env.ENCRYPTION_KEY).update('Akeem').digest(process.env.ENCRYPTION_ENCODING);
 
 module.exports = {
 
